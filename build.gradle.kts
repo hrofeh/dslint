7b8016e7707b9b79a75e8041abe244d2cd3e2a7b
hanan.rofe-haim <hanan.rofehaim@unity3d.com>
2024-04-04 21:42:56 +0300

    Updated gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.kotlin.android) apply false
 }
